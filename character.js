f8dd305db2b29c8f87337cef4198342f34b4dc36
arina-gheorghiu <ghar23bn@student.ju.se>
2024-11-28 20:07:07 +0100

    sidepart character

--- character.js
+++ character.js
@@ -6,6 +6,12 @@ let characterBack;
 let characterBackLF;
 let characterBackRF;
 
+let characterSideRightLF;
+let characterSideRightRF;
+
+let characterSideLeftLF;
+let characterSideLeftRF;
+
 let x = 0;
 let checkIfUp = 0;
 let checkIfDown = 0;
@@ -20,6 +26,11 @@ function preload() {
   characterBack = loadImage("/character/character-back.png");
   characterBackLF = loadImage("/character/character-back move (2).png");
   characterBackRF = loadImage("/character/character-back move.png");
+  characterSideRightLF = loadImage("/character/character-rightside-leftleg.png");
+  characterSideRightRF = loadImage("/character/character-rightside-rightleg.png");
+  characterSideLeftLF = loadImage("/character/character-leftside-leftleg.png");
+  characterSideLeftRF = loadImage("/character/character-leftside-rightleg.png");
+
 }
 
 function draw() {
@@ -49,10 +60,34 @@ function draw() {
     x = x + 1;
     checkIfUp = 1;
     checkIfDown = 0;
-  } else if (checkIfUp === 1) {
+  } else if (checkIfUp === 1 && keyIsDown(39) === false && keyIsDown(37) === false) {
     image(characterBack, characterX, characterY);
-  } else if (checkIfDown === 1) {
+  } else if (checkIfDown === 1 && keyIsDown(39) === false && keyIsDown(37) === false) {
     image(characterFront, characterX, characterY);
+  }
+
+  if (keyIsDown(39) && x <= 5) {
+    image(characterSideRightLF, characterX, characterY);
+    x = x + 1;
+    characterX = characterX + 2;
+ 
+  } else if (keyIsDown(39) && x <= 10 && x > 5) {
+    image(characterSideRightRF, characterX, characterY);
+    characterX = characterX + 2;
+    x = x + 1;
+
+  } else if (keyIsDown(37) && x <= 5) {
+    image(characterSideLeftRF, characterX, characterY);
+    x = x + 1;
+    characterX = characterX - 2;
+
+  } else if (keyIsDown(37) && x <= 10 && x > 5) {
+    image(characterSideLeftLF, characterX, characterY);
+    characterX = characterX - 2;
+    x = x + 1;
+
+    
+
   }
 
   if (x === 10) {
