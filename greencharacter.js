e9cdbfd9fb9772d4a192f17ced175119d9472022
arina-gheorghiu <ghar23bn@student.ju.se>
2024-11-29 09:46:57 +0100

    start movement

--- greencharacter.js
+++ greencharacter.js
@@ -2,23 +2,21 @@ let greencharacterFront;
 let greencharacterBack;
 let greencharacterBackLF;
 let greencharacterBackRF;
-let greencharacterSideLeftLF;
-let greencharacterSideLeftRF;
 let greencharacterX = 100;
-let greencharacterY = 100;
-
+let greencharacterY = 0;
+let x = 0;
+let y = 0;
 
 function preload(){
-  greencharacterFront = loadImage("/character/greencharacter-front.png");
-  greencharacterBack = loadImage("/character/greencharacter-back.png");
-  greencharacterBackLF = loadImage("/character/character-back move (2).png");
-  greencharacterBackRF = loadImage("/character/character-back move.png");
-  characterSideRightLF = loadImage("/character/character-rightside-leftleg.png");
-  characterSideRightRF = loadImage("/character/character-rightside-rightleg.png");
+  greencharacterFront = loadImage("/green character/greencharacter-front.png");
+  greencharacterBack = loadImage("/green character/greencharacter-back.png");
+  greencharacterBackLF = loadImage("/green character/greencharacter-back-leftleg.png");
+  greencharacterBackRF = loadImage("/green character/character-back-rightleg.png");
 }
 
 
 function draw() {
   background(255);
   image(greencharacterBack, greencharacterX, greencharacterY);
- }
+  greencharacterY = greencharacterY - 2;
+}
