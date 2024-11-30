8ad7577b3d6fc9583f2eac812436adc2c3d92071
arina-gheorghiu <ghar23bn@student.ju.se>
2024-11-30 17:44:37 +0100

    red character movement

--- redcharacter.js
+++ redcharacter.js
@@ -1,46 +1,76 @@
-let redCharacterFront;
-let redCharacterBack;
-let redCharacterBackLF;
-let redCharacterBackRF;
-let redCharacterX = 100;
-let redCharacterY = 500;
-let x = 0;
-let y = 0;
+class RedCharacter {
+  constructor(characterX, characterY) {
+    this.characterX = characterX; 
+    this.characterY = characterY; 
+    this.counter = 0; 
+    this.targetX = 200; // The X position where the character should stop
+  }
+
+  preload() {
+    this.characterFront = loadImage("./red character/redcharacter-front.png");
+    this.characterBack = loadImage("./red character/redcharacter-back.png");
+    this.characterBackLF = loadImage(
+      "./red character/redcharacter-back-leftleg.png"
+    );
+    this.characterBackRF = loadImage(
+      "./red character/redcharacter-back-rightleg.png"
+    );
+    this.characterRightSideLF = loadImage(
+      "./red character/redcharacter-rightside-leftleg.png"
+    );
+    this.characterRightSideRF = loadImage(
+      "./red character/redcharacter-rightside-rightleg-28.png"
+    );
+  }
+
+  draw() {
+    // Moving UP
+    if (this.characterY >= 200) {
+      if (this.counter <= 5) {
+        image(this.characterBackRF, this.characterX, this.characterY); 
+      } else {
+        image(this.characterBackLF, this.characterX, this.characterY);
+      }
+      this.characterY -= 2; 
+      this.counter++;
+    }
+
+    // Moving RIGHT until it reaches the targetX
+    else if (this.characterX < this.targetX) { 
+      if (this.counter <= 5) {
+        image(this.characterRightSideLF, this.characterX, this.characterY); 
+      } else {
+        image(this.characterRightSideRF, this.characterX, this.characterY); 
+      }
+      this.characterX += 1; // Move the character to the right
+      this.counter++;
+    } else {
+      // Once the character reaches the targetX, stop and show the backside view
+      if (this.counter <= 5) {
+        image(this.characterBack, this.characterX, this.characterY); 
+      }
+    }
+
+    // Reset counter after every animation cycle
+    if (this.counter === 10) {
+      this.counter = 0;
+    }
+  }
+}
+
+
+let redCharacter;
 
 function preload() {
-  redCharacterFront = loadImage("/red character/redcharacter-front.png");
-  redCharacterBack = loadImage("/red character/redcharacter-back.png");
-  redCharacterBackLF = loadImage(
-    "/red character/redcharacter-back-leftleg.png"
-  );
-  redCharacterBackRF = loadImage(
-    "/red character/redcharacter-back-rightleg.png"
-  );
-  redCharacterRightSideLF = loadImage("/red character/redcharacter-rightside-leftleg.png");
-  redCharacterRightSideRF = loadImage("/red character/redcharacter-rightside-rightleg.png");
+  redCharacter = new RedCharacter(100, 500);
+  redCharacter.preload();
+}
+
+function setup() {
+  createCanvas(800, 600);
 }
 
 function draw() {
   background(255);
-
-  if (redCharacterY >= 200) {
-    image(redCharacterBack, redCharacterX, redCharacterY);
-    redCharacterY = redCharacterY - 2;
-  } else if (redCharacterY < 200 && redCharacterX <= 400 && x <= 5) {
-    image(redcharacterRightSideLF, redCharacterX, redCharacterY);
-    redCharacterX += 1;
-    x += 1;
-  }
-    else if(redCharacterY < 200 && redCharacterX <= 400 && x > 5){
-      image(redcharacterRightSideRF, redCharacterX, redCharacterY);
-      redCharacterX += 1;
-      x += 1;
-    }
-
-if (x === 10) {
-      x = 0;
-    }
-  }
-  
-
-
+  redCharacter.draw();
+}
